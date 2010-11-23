af757a08213a3e668a988d63417bc7061f04f86e
Tim 'mithro' Ansell <mithro@mithis.com>
2010-11-23 22:29:01 +1100

    Making the play page look good.
    (It's all relative right ;)

--- django_extra.py
+++ django_extra.py
@@ -1,10 +1,12 @@
 
 from google.appengine.ext import db
 from django.template.defaultfilters import stringfilter, register
+from django.utils.html import conditional_escape
+from django.utils.safestring import mark_safe
+
 
 @register.filter
 def key(obj):
-
   if not obj:
     return ''
 
@@ -13,3 +15,31 @@ def key(obj):
     return key
   except db.NotSavedError, e:
     return ''
+
+
+@register.filter
+def icon(obj, autoescape=False):
+  if not obj:
+    return ''
+
+  if autoescape:
+    esc = conditional_escape
+  else:
+    esc = lambda x: x
+
+  if hasattr(obj, 'reference'):
+     realobj = obj.reference
+  else:
+     realobj = obj
+
+  return mark_safe("""\
+<div class='icon'>
+  <img src='%(icon)s' alt='%(desc)s'>
+  %(name)s
+</div>
+""" % {'icon': esc(realobj.icon),
+       'desc': esc(realobj.description),
+       'name': esc(realobj.name)})
+
+icon.needs_autoescape = True
+
